c66a345273707603a46ff296695de38cebaca3c6
chandrasoodan <chandrasoodan1996@gmail.com>
2022-07-31 16:23:47 +0530

    tcommit

--- ClosedPrs/VIPER/PrInteractor.swift
+++ ClosedPrs/VIPER/PrInteractor.swift
@@ -6,3 +6,34 @@
 //
 
 import Foundation
+// ref to presenter
+
+protocol AnyInteractor{
+    var presenter: AnyPresenter? {get set}
+    func getClosedPrs()
+}
+
+class PrInteractor: AnyInteractor{
+    var presenter: AnyPresenter?
+    
+    
+    func getClosedPrs() {
+        
+        
+        
+        guard let url = URL(string: "https://api.github.com/repos/firebase/quickstart-ios/pulls?state=closed") else{
+            return
+        }
+        
+        let task = URLSession.shared.closedPrsTask(with: url) {[weak self] prs, res, err in
+            print("mass")
+            print(err)
+        
+            self?.presenter?.prInteractorDidFetchPrs(with: .success(prs ?? []))
+            print(prs?.first?.title)
+        }
+        task.resume()
+                
+    }
+}
+
